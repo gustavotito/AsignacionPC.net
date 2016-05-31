cdcb189c5132f00a1b670a1cbb3233eb949a57f6
gustavotito <gustitoverdi@gmail.com>
2016-05-31 12:28:24 -0500

    pr - servicio Equipo

--- ServicioEquipo/Persistencia/EquiposDAO.cs
+++ ServicioEquipo/Persistencia/EquiposDAO.cs
@@ -9,15 +9,13 @@ namespace ServicioEquipo.Persistencia
 {
     public class EquiposDAO
     {
-
-        private string CadenaConexion = "Data Source=305a4753-766f-4c06-b100-a60c0046e39d.sqlserver.sequelizer.com;Database=db305a4753766f4c06b100a60c0046e39d;Initial Catalog=db305a4753766f4c06b100a60c0046e39d;User Id=oytjmyrazbvivilj; Password=ToLz4HuFLd3g7XqckzCHPQc6QPYDsgzupYqpUqQ8KpTzdHcfTXjyZZNdkPSnSYnE";
-
+       
         public List<Equipos> ListarEquipos()
         {
             List<Equipos> equiposEncontrado = new List<Equipos>();
             Equipos equipoEncontrado = null;
             string sql = "SELECT nro_serie,e_tipo,e_modelo,EQ.e_descripcion AS descripcion,e_fech_registro,id_area,ES.e_descripcion AS estado from T_Equipos AS EQ INNER JOIN T_Estado AS ES ON EQ.id_estado=ES.id_estado";
-            using (SqlConnection conexion = new SqlConnection(CadenaConexion))
+            using (SqlConnection conexion = new SqlConnection(ConexionUtil.CadenaConexion))
             {
                 conexion.Open();
                 using (SqlCommand comando = new SqlCommand(sql, conexion))
@@ -56,7 +54,7 @@ namespace ServicioEquipo.Persistencia
         {
             Equipos equipoEncontrado = null;
             string sql = "SELECT nro_serie,e_tipo,e_modelo,EQ.e_descripcion AS descripcion,e_fech_registro,AR.a_descripcion AS area,ES.e_descripcion AS estado from T_Equipos AS EQ INNER JOIN T_Estado AS ES ON EQ.id_estado=ES.id_estado INNER JOIN T_Area AS AR ON EQ.id_area = AR.id_area WHERE nro_serie=@id";
-            using (SqlConnection conexion = new SqlConnection(CadenaConexion))
+            using (SqlConnection conexion = new SqlConnection(ConexionUtil.CadenaConexion))
             {
                 conexion.Open();
                 using (SqlCommand comando = new SqlCommand(sql, conexion))
